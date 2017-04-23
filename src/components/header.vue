ad6c72202d4437b141bc64cfcc08f2ddb6ff8309
qiang bit <bitqiang@outlook.com>
2017-04-23 23:18:19 +0800

    '配置环境成'

---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <h1>豆瓣</h1>
+    <input type="text">
+    <button>搜索</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'header',
+  data() {
+    return {}
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+.header {
+  display: flex;
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import header from '../components/header'
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  },
+  components:{
+    'v-header':header
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
